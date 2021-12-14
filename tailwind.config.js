a4f82851c7ec9d02dbfd8fafc7eee54ec43fa225
rdnrn <rodina.arina00@gmail.com>
2021-12-14 22:59:37 +0300

    Add tailwind and basic markup for registration form

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
